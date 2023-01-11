42ab8fe20ac6590c9ddad6e470ed97f6338ef501
ChoosingAName <79235987+ChoosingAName@users.noreply.github.com>
2023-01-10 21:00:10 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-song="";
+song1="";
+song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
-    song=loadSound("music.mp3");
+    song1=loadSound("music.mp3");
+    song2=loadSound("song2.mp3");
 }
 function setup(){
     canvas=createCanvas(600,500);
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose",gotposes);
 }
 function draw(){
     image(video,0,0,600,500);
 }
-function play(){
-    song.play();
+function modelLoaded(){
+    console.log("Posenet Is Initialized");
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY"+leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("leftWristX="+rightWristX+"rightWristY"+rightWristY);
+    }
 }
