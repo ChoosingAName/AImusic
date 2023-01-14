6ea0fd07c73c2cb9931e34331ed53d579d2a4157
ChoosingAName <79235987+ChoosingAName@users.noreply.github.com>
2023-01-14 12:45:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+song1_status="";
+song2_status="";
 song1="";
 song2="";
+scoreleftWrist=0;
 leftWristX=0;
 leftWristY=0;
 rightWristX=0;
@@ -18,6 +21,20 @@ function setup(){
 }
 function draw(){
     image(video,0,0,600,500);
+    song1_status=song1.isPlaying();
+    song2_status=song2.isPlaying();
+    fill('#FF0000');
+    stroke('FF0000');
+   if(scoreleftWrist>0.2)
+   {
+    circle(leftWristX,leftWristY,20);
+    song1.stop();
+    if(song2_status==false)
+    {
+        song2.play();
+        document.getElementById('song').innerHTML="playing song 1";
+    }
+   }
 }
 function modelLoaded(){
     console.log("Posenet Is Initialized");
@@ -25,6 +42,7 @@ function modelLoaded(){
 function gotposes(results){
     if(results.length>0){
         console.log(results);
+        scoreleftWrist=results[0].pose.keypoints.score;
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX="+leftWristX+"leftWristY"+leftWristY);
